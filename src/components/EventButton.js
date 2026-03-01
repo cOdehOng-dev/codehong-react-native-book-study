1efbabae65c1239f4f48238aa8b6d9ce76c28873
c0deh0ng <ghdtjrdn0323@gmail.com>
2026-03-01 12:00:00 +0900

    - test

--- src/components/EventButton.js
+++ src/components/EventButton.js
@@ -1,5 +1,13 @@
+import { TouchableOpacity } from "react-native"
 
 
 const EventButton = () => { 
     const _onPressIn = () => console.log('Press In !!!\n')
+    const _onPressOut = () => console.log('Press Out !!!\n')
+    const _onPress = () => console.log('Press !!!\n')
+    const _onLongPress = () => console.log('Long Press !!!\n')
+
+    return (
+        <TouchableOpacity></TouchableOpacity>
+    )
  }
